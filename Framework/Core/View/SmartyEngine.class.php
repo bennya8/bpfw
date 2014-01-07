b18986bd075fad324ab077a45e238839b8b9d35a
bennya8 <benny_a8@live.com>
2014-01-07 16:42:44 +0800

    strip DS constant and improve __constuct method

--- Framework/Core/View/SmartyEngine.class.php
+++ Framework/Core/View/SmartyEngine.class.php
@@ -1,5 +1,5 @@
 <?php
-require SYS_PATH . DS . 'Core/View/Smarty/Smarty.class.php';
+require SYS_PATH . '/Core/View/Smarty/Smarty.class.php';
 
 /**
  * Smarty引擎实现类
@@ -10,32 +10,25 @@ require SYS_PATH . DS . 'Core/View/Smarty/Smarty.class.php';
  */
 class SmartyEngine extends Smarty
 {
-	/**
-	 * Smarty配置
-	 * @access private
-	 * @var array
-	 */
-	private $_smartyConfig = array();
 
 	/**
 	 * 构造方法，初始化配置
 	 * @access public
 	 * @return void
 	 */
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 		$this->muteExpectedErrors();
-		$this->_smartyConfig = Config::Conf('VIEW_CONFIG');
-		$this->left_delimiter = $this->_smartyConfig['SMARTY_LEFT_DELIMITER'];
-		$this->right_delimiter = $this->_smartyConfig['SMARTY_RIGHT_DELIMITER'];
-		$this->force_compile = $this->_smartyConfig['SMARTY_FORCE_COMPILE'];
-		$this->compile_check = $this->_smartyConfig['SMARTY_COMPILE_CHECK'];
-		$this->caching = $this->_smartyConfig['SMARTY_CACHING'];
-		$this->cache_lifetime = $this->_smartyConfig['SMARTY_CACHE_TIME'];
-		$this->setCompileDir($this->_smartyConfig['SMARTY_COMPILE_DIR']);
-		$this->setTemplateDir($this->_smartyConfig['SMARTY_TEMPLATE_DIR']);
-		$this->setCacheDir($this->_smartyConfig['SMARTY_CACHE_DIR']);
+		$config = Config::Get('Smarty');
+		$this->left_delimiter = $config['SMARTY_LEFT_DELIMITER'];
+		$this->right_delimiter = $config['SMARTY_RIGHT_DELIMITER'];
+		$this->force_compile = $config['SMARTY_FORCE_COMPILE'];
+		$this->compile_check = $config['SMARTY_COMPILE_CHECK'];
+		$this->caching = $config['SMARTY_CACHING'];
+		$this->cache_lifetime = $config['SMARTY_CACHE_TIME'];
+		$this->setCompileDir($config['SMARTY_COMPILE_DIR']);
+		$this->setTemplateDir($config['SMARTY_TEMPLATE_DIR']);
+		$this->setCacheDir($config['SMARTY_CACHE_DIR']);
 	}
 }
 
