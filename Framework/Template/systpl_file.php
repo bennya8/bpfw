8eb85332b528281aad9e65a1a2ea72e29286cfc5
bennya8 <benny_a8@live.com>
2014-01-07 16:44:33 +0800

    move to sample folder

--- Framework/Template/systpl_file.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	SYS_PATH . DS . 'Template/conf.php' => APP_PATH . DS . 'Conf/conf.php',
-	SYS_PATH . DS . 'Template/IndexAction.class.php' => APP_PATH . DS . 'Action/IndexAction.class.php',
-	SYS_PATH . DS . 'Template/index.html' => APP_PATH . DS . 'View/Default/Index/index.html'
-);
-?>
