764e1b4a3e4571170cbb7a32003911be36329962
bennya8 <benny_a8@live.com>
2014-01-08 23:30:55 +0800

    format code

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-
 define('DEBUG', true);
 define('ROOT_PATH', str_replace("\\", "/", dirname(__FILE__)));
 
 /* 框架路径，请不要随意修改 */
 define('SYS_PATH', ROOT_PATH . '/Framework');
-define('PUBLIC_PATH', ROOT_PATH  . '/Public');
+define('PUBLIC_PATH', ROOT_PATH . '/Public');
 
 /* 应用路径，可自行修改'App'中的内容，'App'为你的应用目录名 */
 define('APP_PATH', ROOT_PATH . '/App');
@@ -18,4 +17,3 @@ require SYS_PATH . '/Bootstrap.class.php';
 
 /* 应用启动 */
 Boostrap::create()->run();
-?>
