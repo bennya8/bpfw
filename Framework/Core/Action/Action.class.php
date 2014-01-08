36a412d736df106499bc8a817bf7859d3ba3b443
bennya8 <benny_a8@live.com>
2014-01-08 23:50:10 +0800

    add toJson / toAjax method and optimise system message hints

--- Framework/Core/Action/Action.class.php
+++ Framework/Core/Action/Action.class.php
@@ -21,18 +21,16 @@ abstract class Action extends Component
 	 * @access public
 	 * @return void
 	 */
-	public function __construct()
-	{
+	public function __construct() {
 		$this->config = Config::Get('View');
-		$this->view = Application::Create('View');
+		$this->view = Application::Create('View', get_class($this));
 		$this->authorize();
 	}
 
 	/**
 	 * 验证方法，每当创建时会自动调用，默认为空
 	 */
-	public function authorize()
-	{}
+	public function authorize() {}
 
 	/**
 	 * 展示模板
@@ -47,8 +45,7 @@ abstract class Action extends Component
 	 * @example 使用原生PHP模板引擎，最多可传1个参数，分别：$template
 	 * @return void
 	 */
-	public function display($template = null, $cache_id = null, $compile_id = null, $parent = null)
-	{
+	public function display($template = null, $cache_id = null, $compile_id = null, $parent = null) {
 		$this->view->display($template, $cache_id, $compile_id, $parent);
 	}
 
@@ -63,46 +60,66 @@ abstract class Action extends Component
 	 * @example 使用TemplateLite引擎，可传2个参数，分别：$key, $value
 	 * @example 使用原生PHP模板引擎，可传2个参数，分别：$key, $value
 	 */
-	public function assign($key, $value, $nocache = false)
-	{
+	public function assign($key, $value, $nocache = false) {
 		$this->view->assign($key, $value, $nocache);
 	}
 
 	/**
 	 * 跳转到成功提示页面
 	 * @access public
-	 * @param string $msg 提示信息
-	 * @param string $url 跳转URL
-	 * @param number $time 跳转时间
+	 * @param string $message 提示信息
+	 * @param string $status 提示状态
+	 * @param string $url 跳转页面
+	 * @param int $time 返回时间
 	 * @return void
 	 */
-	public function success($msg = null,$url = null, $time = 3)
-	{
-		$this->view->success($msg,$url, $time);
+	public function message($message = null, $status = 'success', $url = null, $time = 3) {
+		if ($status == 'success') {
+			$status = Translate::Get('_OPERATION_SUCCESS_');
+			$template = Config::Get('SYS_SUCCESS_PAGE');
+			$template = empty($template) ? SYS_PATH . '/Template/systpl_page_success.html' : $template;
+		} else {
+			$status = Translate::Get('_OPERATION_ERROR_');
+			$template = Config::Get('SYS_ERROR_PAGE');
+			$template = empty($template) ? SYS_PATH . '/Template/systpl_page_error.html' : $template;
+		}
+		$url = empty($url) ? $_SERVER["HTTP_REFERER"] : $url;
+		if (preg_match('/(\S+):(\S+)/', $template, $match)) {
+			$this->assign('status', $status);
+			$this->assign('message', $message);
+			$this->assign('time', $time);
+			$this->assign('url', $url);
+			$this->display($match[1] . '/' . $match[2]);
+		} else {
+			require $template;
+		}
+		exit();
 	}
 
 	/**
-	 * 跳转到错误提示页面
+	 * 返回带封装格式的Json数据
 	 * @access public
-	 * @param string $msg 提示信息
-	 * @param string $url 跳转URL
-	 * @param number $time 跳转时间
-	 * @return void
+	 * @param string $message
+	 * @param mixed $data
+	 * @param int $status
+	 * @return string Json数据
 	 */
-	public function error($msg = null, $url = null ,$time= 3)
-	{
-		$this->view->error($msg,$url, $time);
+	public function toAjax($message = null, $data = null, $status = 1) {
+		return json_encode(array(
+			'message' => $message,
+			'data' => $data,
+			'status' => $status
+		));
 	}
 
 	/**
-	 * 弹出Alert提示框
+	 * 返回Json数据
 	 * @access public
-	 * @param string $msg 提示信息
-	 * @return void
+	 * @param mixed $data
+	 * @return string Json数据
 	 */
-	public function alert($msg)
-	{
-		$this->view->alert($msg);
+	public function toJson($data) {
+		return json_encode($data);
 	}
 
 	/**
@@ -110,8 +127,7 @@ abstract class Action extends Component
 	 * @access public
 	 * @return string 控制器名称
 	 */
-	public function getControllerName()
-	{
+	public function getControllerName() {
 		return get_class($this);
 	}
 
@@ -120,8 +136,7 @@ abstract class Action extends Component
 	 * @access public
 	 * @return string 模板引擎名称
 	 */
-	public function getViewEngineName()
-	{
+	public function getViewEngineName() {
 		return $this->view->getViewEngineName();
 	}
 
@@ -130,8 +145,7 @@ abstract class Action extends Component
 	 * @access public
 	 * @return object 模板引擎实例
 	 */
-	public function getViewEngine()
-	{
+	public function getViewEngine() {
 		return $this->view->getViewEngine();
 	}
 
@@ -141,8 +155,7 @@ abstract class Action extends Component
 	 * @access public
 	 * @return void
 	 */
-	public function switchViewEngine($name)
-	{
+	public function switchViewEngine($name) {
 		return $this->view->switchViewEngine();
 	}
 }
