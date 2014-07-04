a6f59f64144bc8051e783f28464ca1eaa643106d
bennya8 <benny_a8@live.com>
2014-07-04 22:10:26 +0800

    redis cache, not implement yet

--- Framework/Cache/Adapter/Redis.php
+++ Framework/Cache/Adapter/Redis.php
@@ -1,7 +1,58 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: Benny
- * Date: 4/1/14
- * Time: 10:37 PM
- */ 
+ * Redis Database Implementation Class
+ * @namespace System\Cache\Adapter
+ * @package system.cache.adapter.redis
+ * @author Benny <benny_a8@live.com>
+ * @copyright ©2013-2014 http://github.com/bennya8
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+
+namespace System\Cache\Adapter;
+
+use System\Cache\ICache;
+
+class Redis implements ICache
+{
+    public function get()
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function set()
+    {
+        // TODO: Implement set() method.
+    }
+
+    public function flush()
+    {
+        // TODO: Implement flush() method.
+    }
+
+    public function has()
+    {
+        // TODO: Implement has() method.
+    }
+
+    public function open()
+    {
+        // TODO: Implement open() method.
+    }
+
+    public function close()
+    {
+        // TODO: Implement close() method.
+    }
+
+
+
+    public function select(){
+
+    }
+
+
+    public function
+
+}
