1a350652a21fd66acf037d4fc2417cc1d6e6486a
bennya8 <benny_a8@live.com>
2014-07-04 21:55:03 +0800

    config file, includes all of settings

--- Application/Config/Config.php
+++ Application/Config/Config.php
@@ -1,31 +1,74 @@
 <?php
 
 return array(
-    'system' => array(
-        'baseUrl' => 'www'
-    ),
-
-    'module' => array(
-
-    ),
+    'namespace' => array(),
+    'module' => array(),
 
     'component' => array(
-        'log' => array(),
-        'debug' => array(),
-        'route' => array(),
-        'cache' => array(),
-        'session' => array(),
-        'translate' => array(
-            'default_language' => 'zh-cn'
+        'logger' => array(
+            'level' => '',
+            'size' => 204800,
+            'format' => 'Y-m-d H:i:s',
+            'extension' => '.log'
+        ),
+        'profiler' => array(
+            'level' => ''
+        ),
+        'route' => array(
+            'mode' => 'pathinfo',
+            'defaultModule' => 'site',
+            'defaultController' => 'site',
+            'defaultAction' => 'site',
+            'rules' => array()
+        ),
+        'cache' => array(
+            'adapter' => 'file',
 
+        ),
+        'session' => array(
+            'adapter' => 'file',
+
+        ),
+        'translate' => array(
+            'default' => 'zh-cn'
+
+        ),
+        'security' => array(
+            'enable' => ''
+        ),
+        'database' => array(
+            // mysql,mysqli,pdo,access
+            'adapter' => 'mysql',
+            'server' => array(
+                'master' => array(
+                    'host' => 'localhost',
+                    'port' => 3306,
+                    'username' => 'root',
+                    'password' => 'root',
+                    'charset' => 'utf8',
+                    'database' => '_test_'
+                )
+            ),
+        ),
+        'request' => array(
+            'defaultFilter' => 'addslashed'
+        ),
+        'response' => array(
+            ''
+        ),
+        'view' => array(
+            // 'smarty' , 'template lite'
+            'engine' => 'native',
+            'forceCompile' => true,
+            'templateDir' => '',
+            'enableCache' => '',
+            'cacheTime' => 3600,
+            'cacheDir' => '',
         )
     ),
 
-    'route' => array(
 
-    ),
+    'route' => array(),
 
-    'database' => array(
 
-    )
 );
