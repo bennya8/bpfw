886318c8137b0a3c2b1a4cc42ff4d7e453852465
bennya8 <benny_a8@live.com>
2014-07-19 20:20:24 +0800

    [core]component baseclass,magic methods,di service

--- Framework/Core/Component.php
+++ Framework/Core/Component.php
@@ -1,12 +1,34 @@
 <?php
 
+/**
+ * Component abstract class
+ * @namespace System\Core;
+ * @package system.core.component
+ * @author Benny <benny_a8@live.com>
+ * @copyright ©2012-2014 http://github.com/bennya8
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 namespace System\Core;
 
 abstract class Component
 {
-    public function __construct($name = __CLASS__)
+    /**
+     * Dependency inject container
+     * @var array
+     */
+    private $_di = array();
+
+    /**
+     * Constructor
+     */
+    public function __construct($className = '')
     {
-        $config = $this->getDI('config')->get('component');
+        if (empty($className)) {
+            $className = explode('\\', get_class($this));
+            $className = strtolower(array_pop($className));
+        }
+        $config = $this->getDI('config')->get('component')[$className];
         if (!empty($config) && is_array($config)) {
             foreach ($config as $propKey => $propValue) {
                 if (property_exists($this, $propKey)) {
@@ -16,31 +38,94 @@ abstract class Component
         }
     }
 
+    /**
+     * Get object from di container
+     * @param $name
+     * @return bool
+     */
     protected function getDI($name)
     {
-        return DI::factory()->get($name);
+        if (isset($this->_di[$name])) {
+            return $this->_di[$name];
+        } else {
+            return DI::factory()->get($name);
+        }
     }
 
-
-    protected function setDI($name, $mixed)
+    /**
+     * Set object to di container
+     * @param $name
+     * @param $mixed
+     * @param bool $shared
+     */
+    protected function setDI($name, $mixed, $shared = false)
     {
-        return DI::factory()->set($name, $mixed);
+        if ($shared) {
+            DI::factory()->set($name, $mixed);
+        } else {
+            $this->_di[$name] = $mixed;
+        }
     }
 
-    public function __get($name)
+    /**
+     * Get exists property with given key
+     * @param $key
+     * @return null
+     */
+    public function __get($key)
     {
-
+        return property_exists($this, $key) ? $this->$key : null;
     }
 
-    public function __set($name, $value)
+    /**
+     * Set property with given key and value
+     * @param $key
+     * @param $value
+     */
+    public function __set($key, $value)
     {
+        $this->$key = $value;
     }
 
-    public function __call($class, $args)
+    /**
+     * Unset property with given key
+     * @param $key
+     */
+    public function __unset($key)
     {
+        unset($this->$key);
     }
 
-    public static function __callstatic($class, $args)
+    /**
+     * Invoke method
+     * @param $method
+     * @param $args
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call($method, $args)
     {
+        if (method_exists($this, $method)) {
+            return $this->$method($args);
+        } else {
+            throw new \Exception('invoke no exists method');
+        }
+    }
+
+    /**
+     * Invoke static method
+     * @param $method
+     * @param $args
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function __callstatic($method, $args)
+    {
+        $class = get_called_class();
+        if (method_exists($class, $method)) {
+            return static::$method($args);
+        } else {
+            throw new \Exception('invoke no exists static method');
+        }
     }
 }
