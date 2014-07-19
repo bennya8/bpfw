576e76c0f9ef1f7e3f22dd8db190df03d6c29744
bennya8 <benny_a8@live.com>
2014-07-19 20:17:08 +0800

    [cache]modify redis adapter skeleton

--- Framework/Cache/Adapter/Redis.php
+++ Framework/Cache/Adapter/Redis.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Redis Database Implementation Class
+ * Redis Adapter
  * @namespace System\Cache\Adapter
  * @package system.cache.adapter.redis
  * @author Benny <benny_a8@live.com>
@@ -9,50 +9,103 @@
  * @license http://www.apache.org/licenses/LICENSE-2.0
  */
 
-
 namespace System\Cache\Adapter;
 
-use System\Cache\ICache;
+use System\Cache\Cache;
 
-class Redis implements ICache
+class Redis extends Cache
 {
-    public function get()
+
+    private $_db;
+
+    /**
+     * Fetch cache data with given key
+     * @param $key
+     * @return mixed
+     */
+    public function get($key)
     {
         // TODO: Implement get() method.
     }
 
-    public function set()
+    /**
+     * Write cache data with given key and value
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
+    public function set($key, $value)
     {
         // TODO: Implement set() method.
     }
 
+    /**
+     * Delete cache data with given key
+     * @param $key
+     * @return mixed
+     */
+    public function remove($key)
+    {
+        // TODO: Implement remove() method.
+    }
+
+    /**
+     * Checks if the given key in the cache data
+     * @param $key
+     * @return mixed
+     */
+    public function has($key)
+    {
+        // TODO: Implement has() method.
+    }
+
+    /**
+     * Free all data from cache data
+     * @return mixed
+     */
     public function flush()
     {
         // TODO: Implement flush() method.
     }
 
-    public function has()
-    {
-        // TODO: Implement has() method.
-    }
-
+    /**
+     * Open a cache server connection
+     * @return mixed
+     */
     public function open()
     {
-        // TODO: Implement open() method.
+        if(!class_exists('\\Redis')){
+            throw new \Exception('redis module not install');
+        }
+        $this->_db = new \Redis();
+        $this->_db->connect('localhost');
     }
 
+    /**
+     * Close a cache server connect
+     * @return mixed
+     */
     public function close()
     {
-        // TODO: Implement close() method.
+        return true;
     }
 
 
-
-    public function select(){
-
+    /**
+     * Invoke method
+     * @param $method
+     * @param $args
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call($method, $args)
+    {
+        if (method_exists($this->_db, $method)) {
+            $reflectMethod = new \ReflectionMethod($this->_db, $method);
+            return $reflectMethod->invokeArgs($this->_db, $args);
+        } else {
+            throw new \Exception('invoke no exists method');
+        }
     }
-
-
-    public function
 
 }
