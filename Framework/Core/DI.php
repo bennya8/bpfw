daec32935686efaaa49303b3e85b66002d204c3b
bennya8 <benny_a8@live.com>
2014-07-23 21:59:46 +0800

    [core]modify framework dependency injection

--- Framework/Core/DI.php
+++ Framework/Core/DI.php
@@ -1,13 +1,35 @@
 <?php
 
+/**
+ * Dependency injection
+ * @namespace System\Core;
+ * @package system.core.di
+ * @author Benny <benny_a8@live.com>
+ * @copyright ©2012-2014 http://github.com/bennya8
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 namespace System\Core;
 
 class DI
 {
+
+    /**
+     * Instances container
+     * @var array
+     */
     protected static $_shared = array();
 
+    /**
+     * Self instance
+     * @var object
+     */
     protected static $_handle = null;
 
+    /**
+     * DI factory method
+     * @return object
+     */
     public static function factory()
     {
         if (!isset(self::$_handle) && !self::$_handle instanceof self) {
@@ -16,11 +38,22 @@ class DI
         return self::$_handle;
     }
 
+    /**
+     * Get something from container
+     * @param $name
+     * @return mixed
+     */
     public function get($name)
     {
         return isset(self::$_shared[$name]) ? self::$_shared[$name] : false;
     }
 
+    /**
+     * Set something to container
+     * @param $name
+     * @param $mixed
+     * @return mixed
+     */
     public function set($name, $mixed)
     {
         if (!isset(self::$_shared[$name])) {
@@ -29,20 +62,14 @@ class DI
         return self::$_shared[$name];
     }
 
+    /**
+     * Check whether given name object exists
+     * @param $name
+     * @return object / bool
+     */
     public function has($name)
     {
         return isset(self::$_shared[$name]);
     }
-
-    public function __get($name)
-    {
-        return $this->get($name);
-    }
-
-    public function __set($name, $object)
-    {
-        return $this->set($name, $object);
-    }
-
 
 }
