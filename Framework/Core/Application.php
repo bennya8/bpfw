26d571130f13849a972da2c15f27535b303cd452
bennya8 <benny_a8@live.com>
2014-07-23 21:58:04 +0800

    [core]modify framework application

--- Framework/Core/Application.php
+++ Framework/Core/Application.php
@@ -1,24 +1,40 @@
 <?php
 
+/**
+ * Application
+ * @namespace System\Core;
+ * @package system.core.application
+ * @author Benny <benny_a8@live.com>
+ * @copyright ©2012-2014 http://github.com/bennya8
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 namespace System\Core;
 
 class Application
 {
-    public static $version = 'alpha';
 
-    private $_di = array();
-
+    /**
+     * Application main entrance
+     * @return void
+     */
     public function main()
     {
         $this->registerMainService();
+        Profiler::trace('app_start');
         $this->getDI('event')->notify('app_start');
         $this->registerService();
         $this->getDI('event')->notify('dispatcher_start');
         $this->getDI('route')->dispatcher();
         $this->getDI('event')->notify('dispatcher_end');
         $this->getDI('event')->notify('app_end');
+        Profiler::trace('app_end');
     }
 
+    /**
+     * Register core components
+     * @return void
+     */
     protected function registerMainService()
     {
         switch (strtolower(ENVIRONMENT)) {
@@ -29,7 +45,7 @@ class Application
             default:
                 error_reporting(0);
         }
-//        set_exception_handler(array('System\\Core\\Application', 'exceptionHandler'));
+        set_exception_handler(array('System\\Core\\Application', 'exceptionHandler'));
         set_error_handler(array('System\\Core\\Application', 'errorHandler'));
         require SYSTEM_PATH . 'Core/Loader.php';
         $loader = new Loader();
@@ -55,9 +71,15 @@ class Application
         $this->setDI('route', new Route());
     }
 
+    /**
+     * Register user components, namespaces, modules
+     * @return void
+     */
     protected function registerService()
     {
         $loader = $this->getDI('loader');
+        $loader->import('Common.Constants','.php');
+        $loader->import('Common.Functions','.php');
         $loader->registerNamespace($this->getDI('config')->get('namespace'));
         $components = array_map('ucfirst', array_keys($this->getDI('config')->get('component')));
         $services = array('Logger', 'Profiler', 'Cookie', 'Translate', 'Security');
@@ -65,7 +87,7 @@ class Application
         foreach ($components as $component) {
             if (in_array($component, $services)) {
                 $class = 'System\\Core\\' . $component;
-                $this->setDI($component, new $class);
+                $this->setDI(strtolower($component), new $class);
             } elseif (in_array($component, $factoryServices)) {
                 $class = 'System\\' . $component . '\\' . $component;
                 $this->setDI(strtolower($component), $class::factory());
@@ -77,28 +99,14 @@ class Application
         }
     }
 
-    protected function getDI($name)
-    {
-        if (isset($this->_di[$name])) {
-            return $this->_di[$name];
-        } else {
-            return DI::factory()->get($name);
-        }
-    }
-
-    protected function setDI($name, $mixed, $shared = true)
-    {
-        if (!$shared) {
-            $this->_di[$name] = $mixed;
-        } else {
-            DI::factory()->set($name, $mixed);
-        }
-    }
-
+    /**
+     * Application exception handler
+     * @access protected
+     * @param $exception \Exception
+     * @return void
+     */
     public static function exceptionHandler($exception)
     {
-        echo $exception->getMessage();
-
         switch ($exception->getCode()) {
             case E_ERROR:
             case E_STRICT:
@@ -106,21 +114,69 @@ class Application
             case E_WARNING:
             case E_PARSE:
             case E_NOTICE:
+                echo 'server error: '.$exception->getMessage();
+
+            break;
+            case 404:
+                echo 'page not found';
+                break;
         }
     }
 
-    public static function errorHandler($code, $message)
+    /**
+     * Application error handler
+     * @param $code
+     * @param $message
+     * @throws \Exception
+     * @return void
+     */
+    public static function errorHandler($code = '', $message = '')
     {
         throw new \Exception($message, $code);
     }
 
+    /**
+     * Get instance from di container
+     * @param $name
+     * @return mixed
+     */
+    protected function getDI($name)
+    {
+        return DI::factory()->get($name);
+    }
+
+    /**
+     * Set instance to di container
+     * @param $name
+     * @param $mixed
+     * @return mixed
+     */
+    protected function setDI($name, $mixed)
+    {
+        return DI::factory()->set($name, $mixed);
+    }
+
+    /**
+     * Magic get method
+     * Get instance from di container
+     * @param $name
+     * @return mixed
+     */
     public function __get($name)
     {
         return $this->getDI($name);
     }
 
-    public function __set($name, $value)
+    /**
+     * Magic set method
+     * Set instance to di container
+     * @param $name
+     * @param $mixed
+     * @return mixed
+     */
+    public function __set($name, $mixed)
     {
-        $this->setDI($name, $value);
+        return $this->setDI($name, $mixed);
     }
+
 }
