cfc79d692b26fbaee91b99af40b9daa5b1891f8d
bennya8 <benny_a8@live.com>
2014-08-02 08:25:24 +0800

    [application]sample code

---
+++ Application/i18n/en/common.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'query.success' => 'query success',
+    'query.fail' => 'query failed',
+    'create.success' => 'create success',
+    'create.fail' => 'create failed',
+    'update.success' => 'update success',
+    'update.fail' => 'update failed',
+    'delete.success' => 'delete success',
+    'delete.fail' => 'delete failed',
+);
---
+++ Application/i18n/zh-cn/common.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'query.success' => '查询成功',
+    'query.fail' => '查询失败',
+    'create.success' => '添加成功',
+    'create.fail' => '添加失败',
+    'update.success' => '添加成功',
+    'update.fail' => '添加失败',
+    'delete.success' => '删除成功',
+    'delete.fail' => '删除失败',
+);
