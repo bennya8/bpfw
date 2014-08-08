5ec4b473c0a2f987231179b1bc591c2418eb2f19
bennya8 <benny_a8@live.com>
2014-08-08 09:42:07 +0800

    [core]added model validation

---
+++ Framework/Core/Validation.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace System\Core;
+
+class Validation
+{
+
+    public function required()
+    {
+        return true;
+    }
+
+    public function number()
+    {
+
+    }
+
+    public function email()
+    {
+        return true;
+
+    }
+
+    public function range()
+    {
+        return true;
+
+    }
+
+    public function regex()
+    {
+    }
+
+    public function length()
+    {
+        return true;
+
+    }
+
+}
