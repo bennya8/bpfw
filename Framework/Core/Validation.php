47dec07d2637b9777a25b569f733b8ea7fe1bdb8
bennya8 <benny_a8@live.com>
2014-11-26 02:31:19 +0800

    add docs

--- Framework/Core/Validation.php
+++ Framework/Core/Validation.php
@@ -1,40 +1,79 @@
 <?php
 
+/**
+ * Validation
+ * @namespace System\Core
+ * @package system.core.validation
+ * @author Benny <benny_a8@live.com>
+ * @copyright ©2014 http://github.com/bennya8
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 namespace System\Core;
 
 class Validation
 {
 
-    public function required()
+    /**
+     * Check whether value is not empty
+     * @param $value
+     * @return bool
+     */
+    public function required($value)
     {
         return true;
     }
 
+    /**
+     * Check whether value is a number
+     * @param $value
+     * @return bool
+     */
     public function number()
     {
-
+        return true;
     }
 
-    public function email()
+    /**
+     * Check value with email format
+     * @param $value
+     * @return bool
+     */
+    public function email($value)
     {
         return true;
 
     }
 
-    public function range()
+    /**
+     * Check value is a number in range
+     * @param $value
+     * @return bool
+     */
+    public function range($value, $range)
     {
         return true;
 
     }
 
-    public function regex()
-    {
-    }
-
-    public function length()
+    /**
+     * Check value with a regular express
+     * @param $value
+     * @return bool
+     */
+    public function regex($value, $express)
     {
         return true;
+    }
 
+    /**
+     * Check value with given length
+     * @param $value
+     * @return bool
+     */
+    public function length($value, $length)
+    {
+        return true;
     }
 
 }
