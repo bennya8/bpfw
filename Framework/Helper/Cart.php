78872b5d1984c68caf49c04b59feeeed19de950d
bennya8 <benny_a8@live.com>
2014-11-28 01:38:18 +0800

    add cart helper

--- Framework/Helper/Cart.php
+++ Framework/Helper/Cart.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @todo Cart helper
+ * Cart helper
  * @namespace System\Helper;
  * @package system.helper.cart
  * @author Benny <benny_a8@live.com>
@@ -13,5 +13,113 @@ namespace System\Helper;
 
 class Cart
 {
+
+    /**
+     * Cart items
+     * @property array
+     */
+    protected $cart = array();
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->cart =& $_SESSION['my_cart'];
+    }
+
+    /**
+     * Check whether goods_id exists
+     * @access protected
+     * @param $goods_id
+     * @return bool
+     */
+    protected function exist($goods_id)
+    {
+        return array_key_exists($goods_id, $this->cart);
+    }
+
+    /**
+     * Get goods items
+     * @param int $goods_id [optional]
+     * @return array
+     */
+    public function get($goods_id = 0)
+    {
+        if (empty($goods_id)) {
+            return $this->cart;
+        } else {
+            return $this->exist($goods_id) ? $this->cart[$goods_id] : array();
+        }
+    }
+
+    /**
+     * Add goods item
+     * @access public
+     * @param int $goods_id
+     * @param string $goods_name
+     * @param int $price
+     * @param int $quantity [optional]
+     * @param string $remark [optional]
+     * @return array
+     */
+    public function add($goods_id, $goods_name, $price, $quantity = 1, $remark = '')
+    {
+        if ($this->exist($goods_id)) {
+            $item =& $this->cart[$goods_id];
+            $item['quantity'] = (int)$item['quantity'] + $quantity;
+            $item['remark'] = (string)$remark;
+        } else {
+            $item = array(
+                'goods_id' => (int)$goods_id,
+                'goods_name' => (int)$goods_name,
+                'price' => (int)$price,
+                'quantity' => (int)$quantity,
+                'remark' => (string)$remark
+            );
+            $this->cart[$goods_id] = $item;
+        }
+        return $this->cart;
+    }
+
+    /**
+     * Update goods item
+     * @access public
+     * @param int $goods_id
+     * @param int $quantity [optional]
+     * @param string $remark [optional]
+     * @return array
+     */
+    public function update($goods_id, $quantity = 1, $remark = '')
+    {
+        if ($this->exist($goods_id)) {
+            $item =& $this->cart[$goods_id];
+            $item['quantity'] = (int)$item['quantity'] + $quantity;
+            $item['remark'] = (string)$remark;
+        }
+        return $this->cart;
+    }
+
+    /**
+     * Delete cart item by goods_id
+     * @param int $goods_id
+     * @return array
+     */
+    public function delete($goods_id)
+    {
+        if ($this->exist($goods_id)) {
+            unset($this->cart[$goods_id]);
+        }
+        return $this->cart;
+    }
+
+    /**
+     * Empty cart items
+     * @return array
+     */
+    public function trash()
+    {
+        return $this->cart = array();
+    }
 
 }
